8de6e17f5dd86b577b0abe72fe6f2f45fbfdf6e3
sprockmonty <sprockmonty@gmail.com>
2020-06-18 03:17:35 +0100

    using automatic page segmentation with OSD

--- src/ocrip.sh
+++ src/ocrip.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 screen=$(mktemp --suffix=.png)
 maim "$screen"
-outtext=$(tesseract "$screen" - -l eng  --dpi 70 | awk 'NF')
+outtext=$(tesseract "$screen" - -l eng  --dpi 70 --psm 1 | awk 'NF')
 rm $screen
 echo "$outtext"
